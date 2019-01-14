cda09ed70a37d556d29cb8a2ebe9d7cd9dba6d3e
michaelagard <mdagard@gmail.com>
2019-01-14 06:24:44 -0500

    ng generate module app-routing --flat --module=app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BearsComponent } from './bears/bears.component';
 import { BearDetailComponent } from './bear-detail/bear-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
