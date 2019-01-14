aa5dee973aa17d9d2a525de1eb524e97e690bc61
michaelagard <mdagard@gmail.com>
2019-01-14 06:20:52 -0500

    code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
 import { AppComponent } from './app.component';
 import { BearsComponent } from './bears/bears.component';
 import { BearDetailComponent } from './bear-detail/bear-detail.component';
