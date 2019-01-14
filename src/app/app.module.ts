2ac42ca9935a81af1bbc3b344462484125433933
michaelagard <mdagard@gmail.com>
2019-01-14 05:38:47 -0500

    declare MessageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { BearsComponent } from './bears/bears.component';
 import { BearDetailComponent } from './bear-detail/bear-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BearsComponent,
-    BearDetailComponent
+    BearDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
