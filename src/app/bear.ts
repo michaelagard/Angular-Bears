cbb3b0306493e907e417168a2861e6696535ec3e
michaelagard <mdagard@gmail.com>
2019-01-14 09:34:18 -0500

    add color key and values

--- src/app/bear.ts
+++ src/app/bear.ts
@@ -1,6 +1,7 @@
 export class Bear {
   id: number;
   name: string;
+  color: string;
 }
 
 export default Bear
--- src/app/mock-bears.ts
+++ src/app/mock-bears.ts
@@ -1,14 +1,14 @@
 import { Bear } from './bear';
 
 export const BEARS: Bear[] = [
-  { id: 11, name: 'Ashcoat Bear' },
-  { id: 12, name: 'Balduvian Bears' },
-  { id: 13, name: 'Bear Cub' },
-  { id: 14, name: 'Dragon-Scarred Bear' },
-  { id: 15, name: 'Phantasmal Bear' },
-  { id: 16, name: 'Pale Bears' },
-  { id: 17, name: 'Grizzly Bears' },
-  { id: 18, name: 'Forest Bear' },
-  { id: 19, name: 'Razorclaw Bear' },
-  { id: 20, name: 'River Bear' }
+  { id: 11, name: 'Ashcoat Bear', color: 'green' },
+  { id: 12, name: 'Balduvian Bears', color: 'green' },
+  { id: 13, name: 'Bear Cub', color: 'green' },
+  { id: 14, name: 'Dragon-Scarred Bear', color: 'green' },
+  { id: 15, name: 'Phantasmal Bear', color: 'blue' },
+  { id: 16, name: 'Pale Bears', color: 'green' },
+  { id: 17, name: 'Grizzly Bears', color: 'green' },
+  { id: 18, name: 'Forest Bear', color: 'green' },
+  { id: 19, name: 'Razorclaw Bear', color: 'green' },
+  { id: 20, name: 'Ursine Fylgja', color: 'white' }
 ];
