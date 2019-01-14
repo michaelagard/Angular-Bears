1968ca3df1c5ba490f6fdfaf491e93bb011d6d36
michaelagard <mdagard@gmail.com>
2019-01-14 08:00:20 -0500

    add Dashboard, BearDetail, and paths for each

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { BearsComponent } from './bears/bears.component';
+import { BearDetailComponent } from './bear-detail/bear-detail.component';
+
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: BearDetailComponent },
   { path: 'bears', component: BearsComponent }
 ]
 @NgModule({
-  exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes)]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
