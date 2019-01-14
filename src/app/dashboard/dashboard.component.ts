24b0dfd9d0576378e88f10fa9557fe857f1cd1b9
michaelagard <mdagard@gmail.com>
2019-01-14 08:05:12 -0500

    add getBears method assigned bears to Bear[]

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Bear } from '../bear';
+import { BearService } from '../bear.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  bears: Bear[] = [];
+
+  constructor(private bearService: BearService) { }
 
   ngOnInit() {
+    this.getBears();
   }
 
+  getBears(): void {
+    this.bearService.getBears()
+      .subscribe(bears => this.bears = bears.slice(1, 5));
+  }
 }
