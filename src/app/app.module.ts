2e200f1a5831d164e85164b382e4bd2186f37a35
michaelagard <mdagard@gmail.com>
2019-01-14 08:01:40 -0500

    reordered code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
-import { BearsComponent } from './bears/bears.component';
-import { BearDetailComponent } from './bear-detail/bear-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BearDetailComponent } from './bear-detail/bear-detail.component';
+import { BearsComponent } from './bears/bears.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BearsComponent,
-    BearDetailComponent,
-    MessagesComponent,
-    DashboardComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    BearsComponent,
+    BearDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
